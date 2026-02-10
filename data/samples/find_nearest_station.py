9680deefb4c323c2f408090398245e467d3cc7b4
konrad-te <konrad.tyburcy@gmail.com>
2026-02-10 16:45:52 +0100

    Change logic to fetch data

---
+++ data/samples/find_nearest_station.py
@@ -0,0 +1,28 @@
+[
+  {
+    "id": 2464,
+    "location": {
+      "latitude": 50.508196,
+      "longitude": 19.414377
+    },
+    "locationId": 2464,
+    "address": {
+      "country": "Polska",
+      "city": "Zawiercie",
+      "street": "O\u015bwiatowa",
+      "number": "55",
+      "displayAddress1": "Zawiercie",
+      "displayAddress2": "O\u015bwiatowa"
+    },
+    "elevation": 327.39,
+    "airly": True,
+    "sponsor": {
+      "id": 233,
+      "name": "Zawiercie",
+      "description": "Airly Sensor's sponsor",
+      "logo": "https://cdn.airly.eu/logo/Zawiercie.jpg",
+      "link": None,
+      "displayName": "Zawiercie"
+    }
+  }
+]
